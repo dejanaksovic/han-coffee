c6de5375a26312478bc61d85ec89cfef917d2974
Dejan Aksovic <imdejan00@gmail.com>
2023-06-28 21:50:09 +0200

    Trying it out with different domains

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -63,11 +63,11 @@ const googleAuthhander = async(req, res) => {
     const refreshToken = user.refreshToken
 
     //set coockies
-    res.cookie("accessToken", accessToken, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'none', secure: true})
-    res.cookie("refreshToken", refreshToken, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'none', secure: true})
-    res.cookie("email", user.email, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'none', secure: true})
-    res.cookie("name", user.name, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'none', secure: true})
-    res.cookie("role", user.role, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'none', secure: true})
+    res.cookie("accessToken", accessToken, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'lax', domain: '.onrender.com'})
+    res.cookie("refreshToken", refreshToken, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'lax', domain: '.onrender.com'})
+    res.cookie("email", user.email, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'lax', domain: '.onrender.com'})
+    res.cookie("name", user.name, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'lax', domain: '.onrender.com'})
+    res.cookie("role", user.role, {encode: String, maxAge: 1000*6*10, httpOnly: true, sameSite: 'lax', domain: '.onrender.com'})
 
     //redirect back to client
     res.redirect(process.env.SITE_REDIRECT)
