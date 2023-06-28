82331bcaccade44acb2a68acb559098c45917695
Dejan Aksovic <imdejan00@gmail.com>
2023-06-28 20:39:02 +0200

    Still fumbling with cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -63,11 +63,11 @@ const googleAuthhander = async(req, res) => {
     const refreshToken = user.refreshToken
 
     //set coockies
-    res.cookie("accessToken", accessToken, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
+    res.cookie("accessToken", accessToken, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: true})
     res.cookie("refreshToken", refreshToken, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: true})
-    res.cookie("email", user.email, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
-    res.cookie("name", user.name, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
-    res.cookie("role", user.role, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
+    res.cookie("email", user.email, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: true})
+    res.cookie("name", user.name, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: true})
+    res.cookie("role", user.role, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: true})
 
     //redirect back to client
     res.redirect(process.env.SITE_REDIRECT)
