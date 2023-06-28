67382c73b1a2f22e153d96cf92046acf682994c6
Dejan Aksovic <imdejan00@gmail.com>
2023-06-28 20:35:06 +0200

    I think i might have figured it out

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -63,11 +63,11 @@ const googleAuthhander = async(req, res) => {
     const refreshToken = user.refreshToken
 
     //set coockies
-    res.cookie("accessToken", accessToken, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'lax', domain: '649c5eb71071922e52c0c5f2--dreamy-buttercream-145b3d.netlify.app/'})
+    res.cookie("accessToken", accessToken, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
     res.cookie("refreshToken", refreshToken, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: true})
-    res.cookie("email", user.email, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none'})
-    res.cookie("name", user.name, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none'})
-    res.cookie("role", user.role, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none'})
+    res.cookie("email", user.email, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
+    res.cookie("name", user.name, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
+    res.cookie("role", user.role, {encode: String, maxAge: 1000*6*10, httpOnly: false, sameSite: 'none', secure: false})
 
     //redirect back to client
     res.redirect(process.env.SITE_REDIRECT)
