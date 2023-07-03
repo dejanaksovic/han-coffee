5b1a0cecfa6298ed6f2eab483c642f9c4c0287b5
Dejan Aksovic <imdejan00@gmail.com>
2023-07-03 02:30:38 +0200

    Implemented support for categories

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -5,7 +5,7 @@ const ArticleSchema = mongoose.Schema({
     desc: String,
     price: Number,
     url: String,
-    category: { type: String, enum: ["ESPRESSO", "CEDJENI SOKOVI", "TOPLI NAPICI", "KAFE SA UKUSIMA", "SEJK", "KOLACI"], required: true }
+    category: { type: String, enum: ["ESPRESSO", "CEDJENI SOKOVI", "TOPLI NAPICI", "KAFE SA UKUSIMA", "SEJK", "KOLACI", "SOKOVI I HLADNA PICA"], required: true }
 }, {timestamps: true})
 
 const Article = mongoose.model('Article', ArticleSchema)
